c2a506047580dfca35d72928eac47f8dc0a31ad1
sid <sid_ani21@yahoo.in>
2020-08-20 22:12:27 +0530

    tab1 component count

--- Cogniz/VizProfileEdit_test.py
+++ Cogniz/VizProfileEdit_test.py
@@ -7,21 +7,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 # class VizScript:
 
-# browsers = ["firefox", "chrome", "opera", "edge", "ie"]
-#
-# for browser in browsers:
-#     global driver
-#     if browser == "firefox":
-#         driver = webdriver.Firefox(executable_path="D:/Installed Apps/Webdrivers/geckodriver-v0.26.0-win64/geckodriver.exe")
-#     elif browser == "chrome":
-#         driver = webdriver.Chrome(executable_path="D:/Installed Apps/Webdrivers/chromedriver_win32/chromedriver.exe")
-#     elif browser == "opera":
-#         driver = webdriver.Opera(executable_path="D:/Installed Apps/Webdrivers/operadriver_win64/operadriver.exe")
-#     elif browser == "edge":
-#         driver = webdriver.Edge(executable_path="D:/Installed Apps/Webdrivers/edgedriver_win64/msedgedriver.exe")
-#     elif browser == "ie":
-#         driver = webdriver.Ie(executable_path="D:/Installed Apps/Webdrivers/IEDriverServer_x64_3.150.1/IEDriverServer.exe")
-
 password = "admin12345"
 
 @pytest.fixture(scope="module")
@@ -30,13 +15,18 @@ def test_openBrowser():
 
     global driver
     driver = webdriver.Firefox(executable_path="D:/Installed Apps/Webdrivers/geckodriver-v0.26.0-win64/geckodriver.exe")
+    # driver = webdriver.Chrome(executable_path="D:/Installed Apps/Webdrivers/chromedriver_win32/chromedriver.exe")
+    # driver = webdriver.Opera(executable_path="D:/Installed Apps/Webdrivers/operadriver_win64/operadriver.exe")
+    # driver = webdriver.Edge(executable_path="D:/Installed Apps/Webdrivers/edgedriver_win64/msedgedriver.exe")
+    # driver = webdriver.Ie(executable_path="D:/Installed Apps/Webdrivers/IEDriverServer_x64_3.150.1/IEDriverServer.exe")
+
     # driver.implicitly_wait(30)
     driver.maximize_window()
 
-    yield driver
-
-    driver.implicitly_wait(10)
-    driver.quit()
+    # yield driver
+    #
+    # driver.implicitly_wait(10)
+    # driver.quit()
 
 def test_gotoUrl(test_openBrowser):
     # navigate to the application home page
@@ -100,20 +90,25 @@ def test_editUser(test_openBrowser):
     assert displayed_name == first_name
 
 def test_changePassword(test_openBrowser):
-
+    # Click Settings option in profile tab
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div/button[3]/span").click()
 
+    # Clear the input box and enter the current password
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[1]/div/input").clear()
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[1]/div/input").send_keys(password)
 
+    # clear the input box and enter the new password
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/input").clear()
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/input").send_keys(password)
 
+    # clear the input box and re-enter the new password
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[3]/div/input").clear()
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/div[3]/div/input").send_keys(password)
 
+    # click submit button
     driver.find_element_by_xpath("//*[@id=\"dashboard\"]/div/div[2]/div[2]/div[2]/div/div[2]/div[2]/button/span[1]").click()
 
+    # wait and get the text of toast message
     wait = WebDriverWait(driver, 10)
     toast = wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))).text
     #toast = driver.find_element_by_class_name("Toastify__toast-container Toastify__toast-container--bottom-right").text
@@ -138,7 +133,6 @@ def test_changePassword(test_openBrowser):
 
 
 def test_dashboard(test_openBrowser):
-
     expected_dashboard = "test_board1"
     driver.implicitly_wait(20)
 
@@ -155,12 +149,20 @@ def test_dashboard(test_openBrowser):
 
     assert expected_dashboard == actual_dashboard
 
+def test_countTabs(test_openBrowser):
+    expected_tabs = 3
+    actual_tabs = len(driver.find_elements_by_class_name("MuiTab-wrapper"))
+    assert actual_tabs == expected_tabs
 
-def test_logoutUser(test_openBrowser):
+def test_countComponents_tab1(test_openBrowser):
+    expected_components = 32
+    actual_components = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/div/div/div[2]/div/div[2]/div[2]/div"))
+    assert expected_components == actual_components
 
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/h5").click()
-    driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/button[1]/span[1]").click()
-    actualUrl = driver.current_url
-    expectedUrl = "https://console.cognitensor.com/login"
-    assert actualUrl == expectedUrl
+# def test_logoutUser(test_openBrowser):
+#     driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/h5").click()
+#     driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[2]/button[1]/span[1]").click()
+#     actualUrl = driver.current_url
+#     expectedUrl = "https://console.cognitensor.com/login"
+#     assert actualUrl == expectedUrl
 
